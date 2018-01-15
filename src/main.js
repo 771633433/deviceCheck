5049c4276e10ce00722f93c8ad87042e8a414b3f
lv <771633433@qq.com>
2018-01-15 15:28:03 +0800

    Update main.js
    import './assets/css/iview.css'

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import iView from 'iview'
-import 'iview/dist/styles/iview.css'
+import './assets/css/iview.css'
 
 Vue.use(iView)
 
